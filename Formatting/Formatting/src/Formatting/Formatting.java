f3482014004a64d30bc77f31eab3d61ed98cef12
ChrisSpaulding <ChrisSpaulding1991@gmail.com>
2017-09-09 22:38:33 -0600

    will read in the test file, need to refactor to method

--- Formatting/Formatting/src/Formatting/Formatting.java
+++ Formatting/Formatting/src/Formatting/Formatting.java
@@ -1,18 +1,61 @@
-
+/* used code from 
+https://www.caveofprogramming.com/java/java-file-reading-and-writing-files-in-java.html 
+for reading in a file
+*/
 package Formatting;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 /**
  *
  * @author chris_000
  */
 public class Formatting {
-
+  
     /**
      * @param args could be the file location for the file to run?
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello World!");
+        // The name of the file to open.
+        String fileName = "C:\\Users\\chris_000\\Documents\\School\\PrinciplesofProgramming\\Formatting\\Formatting\\src\\Formatting\\test1.txt";
+       ArrayList<LineOfText> textHolder = new ArrayList();
+       
+       
+        // This will reference one line at a time
+        String line = null;
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            int lineCount=0;
+            while((line = bufferedReader.readLine()) != null) {
+                lineCount++;
+                LineOfText temp = new LineOfText(line,lineCount);
+                textHolder.add(temp);
+                
+            }
+            
+
+            // Always close files.
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'");                  
+            // Or we could just do this: 
+            // ex.printStackTrace();
+        }
     }
+    
+    
     
 }
--- Formatting/Formatting/src/Formatting/LineOfText.java
+++ Formatting/Formatting/src/Formatting/LineOfText.java
@@ -4,10 +4,15 @@ package Formatting;
  * Class that will hold one line of text and it's position in the document 
  * @author chris_000
  */
-class LineOfText {
+
+public class LineOfText {
     private final String text; 
     private final int lineNumber;
-    
+
+    LineOfText(){
+        text="";
+        lineNumber=-1;
+    }
     LineOfText(String text, int lineNumber){
         this.text=text;
         this.lineNumber= lineNumber;
