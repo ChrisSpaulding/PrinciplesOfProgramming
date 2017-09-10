11c9827d8702e0655334c0d9d307f642629c56f2
ChrisSpaulding <ChrisSpaulding1991@gmail.com>
2017-09-09 21:54:31 -0600

    New Formatting project uploaded, it is a Netbeans project

---
+++ Formatting/Formatting/src/Formatting/Formatting.java
@@ -0,0 +1,18 @@
+
+package Formatting;
+
+/**
+ *
+ * @author chris_000
+ */
+public class Formatting {
+
+    /**
+     * @param args could be the file location for the file to run?
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+    
+}
